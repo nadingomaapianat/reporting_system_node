dff355b2c0e713f2dae5558d62f567a490f50239
kareem <kareemtall99@gmail.com>
2025-12-21 10:02:55 +0200

    adding complynow api

--- src/grc/grc.module.ts
+++ src/grc/grc.module.ts
@@ -9,10 +9,11 @@ import { GrcKrisController } from './grc-kris.controller';
 import { GrcKrisService } from './grc-kris.service';
 import { DatabaseService } from '../database/database.service';
 import { UserFunctionAccessService } from '../shared/user-function-access.service';
-
+import { GrcComplyController } from './grc-comply.controller';
+import { GrcComplyService } from './grc-comply.service';
 @Module({
-  controllers: [GrcDashboardController, GrcRisksController, GrcIncidentsController, GrcKrisController],
-  providers: [GrcDashboardService, GrcRisksService, GrcIncidentsService, GrcKrisService, DatabaseService, UserFunctionAccessService],
-  exports: [GrcDashboardService, GrcRisksService, GrcIncidentsService, GrcKrisService],
+  controllers: [GrcDashboardController, GrcRisksController, GrcIncidentsController, GrcKrisController, GrcComplyController],
+  providers: [GrcDashboardService, GrcRisksService, GrcIncidentsService, GrcKrisService, GrcComplyService, DatabaseService, UserFunctionAccessService],
+  exports: [GrcDashboardService, GrcRisksService, GrcIncidentsService, GrcKrisService, GrcComplyService],
 })
 export class GrcModule {}
